efef91c82869328777f676e8bdeedf732227167f
TrigonaMinima <shivam_r@outlook.com>
2026-02-06 03:30:39 +0400

    Update: Tailwind CSS in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   // base: '', // For development, we can use the default base path
   base: '/EnergyUnits/',
   build: {
