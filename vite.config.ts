be2231e1f577e7ab0b7ce8cb8163cfb0af878d8f
TrigonaMinima <shivam_r@outlook.com>
2026-02-06 03:17:42 +0400

    Add: icon

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '', // For development, we can use the default base path
   base: '/EnergyUnits/',
   build: {
     outDir: "build/",
